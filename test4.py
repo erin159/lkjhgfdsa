c8432dcdb9b4f960e912e3ed672ead35824579b1
Erin <erin3f3n@gmail.com>
2025-03-27 12:04:55 +1300

    test4

--- test4.py
+++ test4.py
@@ -1,1 +1,16 @@
-print("hello world")
+# First name and age 
+first_name =input("What is your name? ")
+age =input("How old are you? ")
+# Choosing a activity to do 
+print("Choose an activity")
+print("1. Music Jam Session (2 hours, easy $5 fee)")
+print("2. Science Experiments Lab (3 hours, moderate, $10 fee)")
+print("3. Sports Leadership Training (4 hours, challenging, $12 fee)")
+input("Enter the number of your chosen actvity:")
+# Chossing Meal options 
+print("Meal options:")
+print("1. Standard")
+print("2. Vegetarian")
+print("3. Dairy-free")
+print("4. No Meat")
+input("Enter the number of your meal choice:")
